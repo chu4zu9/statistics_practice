07e98b85d0cdae7985f644b14f02589c8dac0159
上杉 廉 <r_uesugi@fukui.audio-technica.local>
2020-11-17 13:11:17 +0900

    Add: パラメータの入力欄を追加.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [valueTable, setValueTable] = useState([
+    {
+      xColumn: "0",
+      yColumn: "0",
+    },
+  ]);
+
+  const handleAddRow = () => {
+    const newRow = { xColumn: "0", yColumn: "0" };
+    setValueTable([...valueTable, newRow]);
+  };
+
+  const handleDeleteRow = () => {
+    const _copiedValueTable = [...valueTable];
+    _copiedValueTable.pop();
+    setValueTable(_copiedValueTable);
+  };
+
+  const handleChangeInputValue = (index, key) => (e) => {
+    const newTable = [...valueTable];
+    newTable[index] = {
+      ...valueTable,
+      [key]: e.target.value,
+    };
+    setValueTable(newTable);
+  };
+
+  var t = valueTable.map((value, index) => {
+    return (
+      <tr key={index}>
+        <td>{index + 1}</td>
+        <td>
+          <input
+            onChange={handleChangeInputValue(index, "xColumn")}
+            value={value.xColumn}
+          />
+        </td>
+        <td>
+          <input
+            onChange={handleChangeInputValue(index, "yColumn")}
+            value={value.yColumn}
+          />
+        </td>
+      </tr>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>{"No."}</td>
+            <td>{"x"}</td>
+            <td>{"y"}</td>
+          </tr>
+        </thead>
+        <tbody>{t}</tbody>
+      </table>
+      <button onClick={() => handleAddRow()}>Add</button>
+      <button onClick={() => handleDeleteRow()}>Delete</button>
     </div>
   );
-}
+};
 
 export default App;
