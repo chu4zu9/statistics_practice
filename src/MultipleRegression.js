e9ffe1ad3b4613d028d4f518eaf2547c3803648d
上杉 廉 <r_uesugi@fukui.audio-technica.local>
2020-11-27 11:15:07 +0900

    Add: Added the ability to view loaded CSV files.

--- src/MultipleRegression.js
+++ src/MultipleRegression.js
@@ -1,31 +1,98 @@
 import React, { useState } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+var FileData = null;
+
+const DataToArraySeparatedNewLine = (data) => {
+  return data.split("\n");
+};
+
+const DataToArraySeparatedComma = (data) => {
+  return data.split(",");
+};
+
+const TakeDataFromCsvData = (csvData) => {
+  const separatedByNewLine = DataToArraySeparatedNewLine(csvData);
+
+  const result = [];
+  for (var i = 0; i < separatedByNewLine.length; i++) {
+    result[i] = DataToArraySeparatedComma(separatedByNewLine[i]);
+  }
+  result.shift();
+
+  return result;
+};
+
+const TakeNonIdDataFromCsvData = (csvData) => {
+  return TakeDataFromCsvData(csvData).map((array) => {
+    array.shift();
+    return array;
+  });
+};
+
+const TakeHeaderFromCsvData = (csvData) => {
+  return DataToArraySeparatedComma(DataToArraySeparatedNewLine(csvData)[0]);
+};
 
 const MultipleRegression = () => {
-  const csvToArray = (csvData) => {
-    var separatedByNewLine = csvData.split("\n");
-    var result = new Array();
-    for (var i = 0; i < separatedByNewLine.length; i++) {
-      result[i] = separatedByNewLine[i].split(",");
-    }
-    return result;
-  };
+  const [gridDatas, setGridDatas] = useState({
+    columns: [{ field: "id" }, { field: "x1" }, { field: "x2" }, { field: "y" }],
+    rows: [
+      { id: 1, x1: 0, x2: 0, y: 0 },
+      { id: 2, x1: 0, x2: 0, y: 0 },
+      { id: 2, x1: 0, x2: 0, y: 0 },
+    ],
+  });
+
   const handleFiles = () => (e) => {
+    console.log("Called handleFiles");
     if (e.target.files.length === 0) {
+      FileData = null;
+      setGridDatas({ columns: {}, rows: {} });
       return;
     }
+
     var reader = new FileReader();
     reader.onload = () => {
-      var data = csvToArray(reader.result);
-      console.log(data);
+      FileData = reader.result;
+      UpdateDataGrid(FileData);
     };
 
     reader.readAsText(e.target.files[0]);
   };
 
+  const UpdateDataGrid = (csvFile) => {
+    const columns = TakeHeaderFromCsvData(csvFile).map((value) => {
+      return { field: value };
+    });
+
+    const rows = [{}];
+    for (var i = 0; i < TakeDataFromCsvData(csvFile).length; i++) {
+      const row = {};
+      for (var j = 0; j < TakeHeaderFromCsvData(csvFile).length; j++) {
+        const key = TakeHeaderFromCsvData(csvFile)[j];
+        row[key] = parseInt(TakeDataFromCsvData(csvFile)[i][j]);
+      }
+      rows[i] = row;
+    }
+    console.log(columns);
+    console.log(rows);
+    setGridDatas({ columns: columns, rows: rows });
+  };
+
+  console.log("render");
+  console.log(gridDatas.columns);
+  console.log(gridDatas.rows);
   return (
     <div>
       重回帰分析
-      <input type="file" onChange={handleFiles()} />
+      <p>
+        <input type="file" onChange={handleFiles()} />
+      </p>
+      {FileData !== null && (
+        <div style={{ height: 400, width: "100%" }}>
+          <DataGrid rows={gridDatas.rows} columns={gridDatas.columns} pageSize={5} />
+        </div>
+      )}
     </div>
   );
 };
